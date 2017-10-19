2da1e86085b3323326c3be1bec513af0713e8971
ramillim <ramil.lim@gmail.com>
2017-10-19 12:50:22 -0400

    Add validations and age_in_days calculation for Pet model.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,18 @@
 class Pet < ApplicationRecord
+  attr_accessor :name, :birth_date
+
+  validates :name, presence: true
+  validate :birth_date_is_not_in_future
+
+  def age_in_days
+    (Time.zone.today - birth_date.to_date).to_i
+  end
+
+  private
+
+  def birth_date_is_not_in_future
+    return unless birth_date
+    return if birth_date.to_date < Time.zone.today
+    errors.add(:birth_date, 'cannot be in the future')
+  end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a name' do
+    invalid_pet = Pet.new
+
+    expect(invalid_pet).to_not be_valid
+    expect(invalid_pet.errors.messages[:name]).to include("can't be blank")
+  end
+
+  it 'validates a birth_date in the past' do
+    valid_pet = Pet.new(name: 'Rover', birth_date: 2.days.ago)
+
+    expect(valid_pet).to be_valid
+  end
+
+  it 'validates that birth_date is not in the future' do
+    invalid_pet = Pet.new(name: 'Rover', birth_date: 1.day.from_now)
+
+    expect(invalid_pet).to_not be_valid
+    expect(invalid_pet.errors.messages[:birth_date])
+      .to include('cannot be in the future')
+  end
+
+  it 'calculates age in days' do
+    birth_date = 45.days.ago
+    pet = Pet.new(name: 'Rover', birth_date: birth_date)
+
+    expect(pet.age_in_days).to eq(45)
+  end
 end
