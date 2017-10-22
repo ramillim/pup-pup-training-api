cabd3112383e5a066b7d9ca02f15da0ffc0d053b
ramillim <ramil.lim@gmail.com>
2017-10-22 16:34:51 -0400

    Fix some rubocop style issues in rubocop_todo

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require_relative 'support/request_helpers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -42,7 +42,7 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace('gem name')
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include AuthTokenHelpers, type: :request
