aecfe192e5e1ad389f437dd13fbcd432b6a3c4ed
ramillim <ramil.lim@gmail.com>
2017-10-24 23:08:08 -0400

    Create BehaviorsController skeleton to get routing tests to pass.

---
+++ app/controllers/api/v1/behaviors_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class BehaviorsController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_behavior, only: [:show, :update, :destroy]
+      before_action :set_pet, only: [:index, :create]
+
+      private
+
+      def set_behavior
+        @behavior = Behavior.find(params[:id])
+      end
+
+      def set_pet
+        @pet = Pet.where(uesr_id: current_user.id).find(params[:pet_id])
+      end
+
+      def behavior_params
+        params.require(:behavior).permit(:name)
+      end
+
+      def render_bad_request
+        render_model_errors(@behavior, :bad_request)
+      end
+    end
+  end
+end
