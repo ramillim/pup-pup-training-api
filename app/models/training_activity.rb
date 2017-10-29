38c4f4a6acf99ac93c0317df3687112f5377aeb9
ramillim <ramil.lim@gmail.com>
2017-10-29 00:01:36 -0400

    Add time validation tests for TrainingActivity trained_at and localization for validates_timeliness gem

--- app/models/training_activity.rb
+++ app/models/training_activity.rb
@@ -2,6 +2,8 @@ class TrainingActivity < ApplicationRecord
   belongs_to :behavior
 
   validates :trained_at, presence: true
+  validates_datetime :trained_at, on_or_before: lambda { Time.current }
+
   validates :training_duration, presence: true,
                                 numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/training_activity_spec.rb
+++ spec/models/training_activity_spec.rb
@@ -11,20 +11,35 @@ describe TrainingActivity, type: :model do
     it { is_expected.to be_valid }
   end
 
-  describe '#trained_at' do
+  describe 'validates #trained_at' do
     subject { TrainingActivity.new }
+    let(:current_time) { Time.current.strftime('%F %T') }
 
-    it 'is required' do
+    before { Timecop.freeze }
+    after { Timecop.return }
+
+    it 'cannot be blank' do
       subject.valid?
 
       expect(subject.errors['trained_at']).to include("can't be blank")
     end
 
     it 'cannot be in the future' do
-      subject.trained_at = 3.days.from_now
+      subject.trained_at = 1.second.from_now
       subject.valid?
 
-      expect(subject.errors['trained_at']).to include("cannot be in the future")
+      expect(subject.errors['trained_at'])
+        .to include("must be equal to or before #{current_time}")
+    end
+
+    it 'can be on or before the current time' do
+      subject.trained_at = Time.current
+      subject.valid?
+      expect(subject.errors['trained_at']).to be_empty
+
+      subject.trained_at = 1.second.ago
+      subject.valid?
+      expect(subject.errors['trained_at']).to be_empty
     end
   end
 end
